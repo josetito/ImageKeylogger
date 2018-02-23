d90aa380b7a34507381e2d5d4b85c26a17bc13c3
josetito <jrtito10@hotmail.com>
2018-02-23 13:54:21 -0300

    pruebas con el raton y las capturas

---
+++ mouse.py
@@ -0,0 +1,36 @@
+from pynput import mouse
+
+def on_move(x, y):
+    print('Pointer moved to {0}'.format(
+        (x, y)))
+
+def on_click(x, y, button, pressed):
+    button = str(button)
+    if pressed:
+        if button == 'Button.left':
+            print('Mousebutton_left {0} at {1} Pressed'.format(x,y))
+        if button == 'Button.right':
+            print('Mousebutton_right {0} at {1} Pressed'.format(x,y))
+        if button == 'Button.middle':
+            print('Mousebutton_middle {0} at {1} Pressed'.format(x,y))
+    else:
+        if button == 'Button.left':
+            print('Mousebutton_left {0} at {1} Release'.format(x,y))
+        if button == 'Button.right':
+            print('Mousebutton_right {0} at {1} Release'.format(x,y))
+        if button == 'Button.middle':
+            print('Mousebutton_middle {0} at {1} Release'.  format(x,y))
+
+def on_scroll(x, y, dx, dy):
+    if dy < 0:
+        print('Scrolled {0} at {1} down'.format((x,y)))
+    else:
+        print('Scrolled {0} at {1} up'.format((x,y)))
+
+
+# Collect events until released
+with mouse.Listener(
+        on_click=on_click,
+        on_move=on_move,
+        on_scroll=on_scroll) as listener:
+    listener.join()
--- onclickimagecapture.py
+++ onclickimagecapture.py
@@ -8,11 +8,12 @@ print str(width) +"x"+ str(height)
 
 
 class Point:
-    def __init__(self,*args, **kwargs):
+    def __init__(self, x=0, y=0):
         # Collect events until released
-        print "entre aca"
-        with mouse.Listener(on_move=self.on_move,on_click=self.on_click, on_scroll=self.on_scroll) as listener:
-            listener.join()
+        self.x = x
+        self.y = y
+        print "soy el x y el y: ",x,y
+        
 
     def on_click(self,x, y, button, pressed):
         button = str(button)
@@ -29,14 +30,31 @@ class Point:
             if button == 'Button.right':
                 print('Mousebutton_right {0} at {1} Release'.format(x,y))
             if button == 'Button.middle':
-                print('Mousebutton_middle {0} at {1} Release'.  format(x,y))
+                print('Mousebutton_middle {0} at {1} Release'.format(x,y))
+
+        return (x,y)
 
     def on_scroll(self, x, y, dx, dy):
-        print('Scrolled {0} at {1}'.format(
-            'down' if dy < 0 else 'up',
-            (x, y)))
+        if dy < 0:
+            print('Scrolled {0} at {1} down'.format(x,y))
+        else:
+            print('Scrolled {0} at {1} up'.format(x, y))
+        return (x,y)
+
     def on_move(self, x, y):
         print('Pointer moved to {0}'.format((x, y)))
+        return(x,y)
 
 
-x = Point()
+x = Point(2,4)
+
+#with mouse.Listener(on_move=self.on_move,on_click=self.on_click, on_scroll=self.on_scroll) as listener:
+ #           listener.join()
+
+'''
+            try:
+                listener.join()
+            except MyException as e:
+                print('{0} was clicked'.format(e.args[0]))
+            
+'''
